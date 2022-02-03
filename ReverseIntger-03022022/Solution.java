1ec4bc83da290147046a3e28d505c7693780c34d
vishesh <dahndhalavishesh@gmail.com>
2022-02-03 23:18:21 +0530

    Added Solution to ReverseIntger

---
+++ ReverseIntger-03022022/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int reverse(int x) {
+       if(x < Math.pow(-2,31) || x > (Math.pow(2,31) - 1)){
+            return 0;
+        }
+        
+        StringBuilder str = new StringBuilder(String.valueOf(x));
+        String st = str.reverse().toString();
+        
+        boolean flag = false;
+        if(st.charAt(st.length() - 1) == '-'){
+            st = st.substring(0,st.length() - 1);
+            flag = true;
+        }
+        
+        int number = Integer.parseInt(st);
+        return number;
+    }      
+}
