f05eb8c3de5ca543122331fa224a54f1971506bd
vishesh <dahndhalavishesh@gmail.com>
2022-03-08 00:06:59 +0530

    Adding Solution to Pluse One

---
+++ PluseOne-07032022/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        String s;
+        for (int i=0; i<digits.length-1 ;i++){
+             s=""+digits[i];
+        }
+        int se = Integer.parseint(s);
+        se+=1;
+        String Ans=se+"";
+        for(int i=0;i<Ans.length();i++){
+            int[] So=Integer.parseint(ans.charAt(1));
+        }
+        return So;
+    }
+}
