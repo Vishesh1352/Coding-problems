16153e48d4299592692d7500bde98bde17f62113
vishesh <dahndhalavishesh@gmail.com>
2022-03-13 19:54:08 +0530

    Adding solution to Ugly number

---
+++ Uglynumber-13032022/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isUgly(int n) {
+        boolean Ans=true;
+        Ans=findUgly(n);
+       return Ans; 
+    }
+    public boolean findUgly(int n){
+        boolean Sol;
+        if(n==1){
+            return Sol=true;
+        }
+        
+        int h=n/2;
+        if(h!=3||h!=5){
+            return Sol=false;
+        }
+        else{
+            return Sol=true;
+        }
+    }
+}
