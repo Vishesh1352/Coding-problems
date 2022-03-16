c43e3c5ed1f3b3977987008946f3a88b71c9e58b
vishesh <dhandhalavishesh@gmail.com>
2022-03-16 21:05:25 +0530

    Adding Solution to sqrt(x)

--- sqrt(x)-09032022/Solution.java
+++ sqrt(x)-09032022/Solution.java
@@ -3,18 +3,18 @@ class Solution {
           root(x);
         return x;
     }
-    public static void main (args []){
-          
-        }
+    public static void main (String[] args){
+           int sqr=0;   
+                }
      public int root(int y){
             
             for (int i =1 ;i<=y/2;i++){
-              int z=y/i;
-             if(z==i){
-               y=z;
-                    }
+                    int z=y/i;
+                    if(z==i){
+                                 y=z;
+                            }
                  
-                }
+                } 
              return y;
              }
 }
