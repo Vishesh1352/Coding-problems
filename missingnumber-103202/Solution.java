959f74e22b2c131ba33985855a215f16ad9b38f6
vishesh <dhandhalavishesh@gmail.com>
2022-03-18 12:44:24 +0530

    Adding solution to missing Number

--- missingnumber-103202/Solution.java
+++ missingnumber-103202/Solution.java
@@ -2,7 +2,7 @@ class Solution {
     public static int missingNumber(int[] nums) {
         int size = nums.length;
         int ans=0;
-       for(int i=0;i<nums.length;i++){
+       for(int i=0;i<nums.length+1;i++){
            for(int j=0;j<size;j++){
                if(i==nums[j]){
                    break;
