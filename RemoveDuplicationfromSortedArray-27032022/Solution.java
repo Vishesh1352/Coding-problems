0102fb9459096059aae07daf6b933bbc9190c042
vishesh <dhandhalavishesh@gmail.com>
2022-03-27 16:38:34 +0530

    Adding solution to Remove Duplication from Sorted Array

---
+++ RemoveDuplicationfromSortedArray-27032022/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int count =0;
+        int index =0;
+        int index2=1;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[index]==nums[index2]){
+                count++;
+                
+            }
+            else{
+                nums[index]=nums[index2];
+            }
+            index++;
+            index2++;
+        }
+        return nums.length-count;
+    }
+}
