55c0ad8b43d2dc3fc8b3c30ab4829db401908696
vishesh <dhandhalavishesh@gmail.com>
2022-03-28 19:55:05 +0530

    Adding solution to Ugly number

--- Uglynumber-13032022/Solution.java
+++ Uglynumber-13032022/Solution.java
@@ -1,22 +1,17 @@
 class Solution {
     public boolean isUgly(int n) {
         boolean Ans;
-        Ans=findUgly(n);
-       return Ans; 
-    }
-    public boolean findUgly(int n){
-        boolean jab;
-        if(n==1||n==2||n==3||n==5||n/2==3||n/2==5||n/3==2||n/3==5||n){
-            return jab=true;
+        if(n==1||n==2||n==3||n==5){
+            return Ans=true;
         }
-        
-        int h=n/2;
-
-        if(h/3!=1 || h/5!=1){
-            return jab=false;
+        int byt=n/2;
+        int byth=n/3;
+        int byf=n/5;
+        if(byt==3||byt==5||byth==2||byth==5||byf==2||byf==3){
+            return Ans=true;
         }
         else{
-            return jab=true;
-        }
-    }
+            return Ans=false;
+        }    
+    }  
 }
