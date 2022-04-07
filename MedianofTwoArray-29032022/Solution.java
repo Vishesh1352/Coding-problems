d4cd971a07dbe35862c0b48d2ee2a7cc78575568
vishesh <dhandhalavishesh@gmail.com>
2022-04-07 23:39:19 +0530

    Adding solution to Median of two Array

--- MedianofTwoArray-29032022/Solution.java
+++ MedianofTwoArray-29032022/Solution.java
@@ -1,14 +1,14 @@
 public class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        double size1=num1.length;
-        double size2=num2.length;
+        double size1=nums1.length;
+        double size2=nums2.length;
         double size=size1+size2;
         double median=0;
         if(size%2==0){
-            median=num1[size/2];
+            median=nums1[size/2];
         }
         else{
-            median=num1[((size+1/2)+(size/2))/2];
+            median=nums1[((size+1/2)+(size/2))/2];
         }
     }
 }
