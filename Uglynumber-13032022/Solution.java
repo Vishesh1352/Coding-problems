45647151704df38c13a8d393abc561eab2f622b6
vishesh <dhandhalavishesh@gmail.com>
2022-04-21 21:43:12 +0530

    Solution to ugly number

--- Uglynumber-13032022/Solution.java
+++ Uglynumber-13032022/Solution.java
@@ -4,5 +4,14 @@ class Solution {
         if(n==1||n==2||n==3||n==5){
             return Ans=true;
         }
-        
+        int byt=n/2;
+        int byth=n/3;
+        int byf=n/5;
+        if(byt==3||byt==5||byth==2||byth==5||byf==2||byf==3){
+            return Ans=true;
+        }
+        else{
+            return Ans=false;
+        }    
+    }         
 }
