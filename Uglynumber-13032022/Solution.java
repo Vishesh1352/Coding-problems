ea4f81a6660e29ab4cc14766d01b38db3bfa7b17
vishesh <dhandhalavishesh@gmail.com>
2022-05-08 22:04:14 +0530

    Adding Solution to Uglynumber

--- Uglynumber-13032022/Solution.java
+++ Uglynumber-13032022/Solution.java
@@ -3,7 +3,12 @@ class Solution {
         boolean Ans=true;
         if(n==1||n==2||n==3||n==5){
             return Ans=true;
-       
+        }
+        int byt=n/2;
+        int byth=n/3;
+        int byf=n/5;
+        if(byt==3||byt==5||byth==2||byth==5||byf==2||byf==3){
+            return Ans=true;
         }
         else{
             return Ans=false;
