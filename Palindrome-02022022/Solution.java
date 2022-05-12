47478766e64c1ae726fb8d9a5d03c00c9871dda0
vishesh <dhandhalavishesh@gmail.com>
2022-05-12 21:07:02 +0530

    Adding Solution to Palindrom

--- Palindrome-02022022/Solution.java
+++ Palindrome-02022022/Solution.java
@@ -4,7 +4,9 @@ class Solution {
         int start = 0;
         int end = str.length() -1;
         while(start<end){
-           
+            if(str.charAt(start++) != str.charAt(end--)){
+                return false;
+                }
         } 
                     return true;
     }
