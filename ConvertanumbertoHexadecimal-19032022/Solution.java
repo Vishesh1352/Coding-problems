2805afe36ff2a9e9ca3427c221a9887d5474bb5c
vishesh <dhandhalavishesh@gmail.com>
2022-05-16 22:13:03 +0530

    Adding solution to Convert a number to Hexadecimal

--- ConvertanumbertoHexadecimal-19032022/Solution.java
+++ ConvertanumbertoHexadecimal-19032022/Solution.java
@@ -1,7 +1,13 @@
 class Solution {
     public static String toHex(int num) {
                 String Ans="";
-                
+                while(num/16!=0){
+                    int rem=num%16;
+                    num=num/16;
+                    if(rem>=10){
+                          
+                    }
+                    Ans=""+rem;
                 }
                 return Ans; 
             }
