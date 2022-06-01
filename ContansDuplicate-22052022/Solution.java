b3d2e84a6ac7e5d432f2ae4b062a286e56ae4c78
vishesh <dhandhalavishesh@gmail.com>
2022-06-01 08:29:29 +0530

    Adding Solution to contans Duplicate

--- ContansDuplicate-22052022/Solution.java
+++ ContansDuplicate-22052022/Solution.java
@@ -2,16 +2,20 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         int size= nums.length;
         boolean ans=true;
+        int count=0;
         for(int i=0;i<size-1;i++){
             for(int j=size-1;j>=0;j--){
+                if(i==j){continue;}
                 if(nums[i]==nums[j]){
-                    ans=true;
-                    break;
-                }
-                else{
-                    ans=false;
+                    count++;
                 }
             }
+        }
+        if(count==0){
+            ans=true;
+        }
+        else{
+            ans=false;
         }
         return ans;
     } 
